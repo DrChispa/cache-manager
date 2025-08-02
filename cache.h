2e5cbb83166f7d3ad5f1fffaa5bfbe7b0dddf35e
emiliano <emiliano.d.sandoval1@gmail.com>
2025-08-02 21:40:40 +0000

    Funciones y Clases declaradas

---
+++ cache.h
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <fstream>
+#include <map>
+#include <utility>
+#include <string>
+
+using namespace std;
+
+template<class T>
+class CacheManager {
+  //members (private)
+  int capacity;
+  map<string, pair<T, int>> cache_data; 
+  // <Clave, <Obj, Indice de Uso>>
+  //,
+  bool write_file (string, T);
+
+public:
+  CacheManager(int);
+  ~CacheManager();
+  
+  void insert (string key, T obj);
+  T get (string key);
+
+};
+
+template <class T>
+CacheManager<T>::CacheManager(int cap) {
+  capacity = cap;
+}
+
+template <class T>
+CacheManager<T>::~CacheManager() {}
+template <class T>
+bool CacheManager<T>::write_file (string key, T obj) {
+return true;
+}
+template <class T>
+void CacheManager<T>::insert (string key, T obj) {
+  // Si la clave existe, actualizamos
+  auto it = cache_data.find(key);
+  if (it != cache_data.end()) {
+    it -> second.first = obj;
+    it -> second.second = 0;
+  } else {
+    // si la cache esta llena, eliminamos el LRU
+    if (cache_data.size() >= capacity) {
+      string lru_key;
+      int max_mru = -1;
+      //buscamos el elemento con el MRU mas alto(el menos reciente)
+      for (const auto& pair : cache_data) {
+        if (pair.second.second > max_mru) {
+          max_mru = pair.second.second;
+          lru_key = pair.first;
+        }
+      }
+      cache_data.erase(lru_key);
+    }
+    //insertamos el nuevo elemento con MRU = 0 (el mas reciente)
+cache_data[key] = make_pair(obj, 0);
+}
+  // actualizaos el MRU de todos los elementos
+  for (auto& pair : cache_data) {
+    pair.second.second++;
+  }
+
+  write_file(key, obj);
+}
+template<class T>
+T CacheManager<T>::get (string key) {}
--- main.cpp
+++ main.cpp
@@ -1,4 +1,29 @@
-int main (int argc, char *argv[]) {
-  
+#include "cache.h"
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+class Student {
+  int id;
+  int value;
+  const char* data;
+public:
+  static const string class_name;
+
+  Student(int _key, int _value, const char* _data): id (_key),value (_value),data (_data) {}
+  void print(){
+    cout << "Student Object: " << id << ", " << value << ", " << data << endl;
+  }
+  ~Student () {};
+};
+
+const string Student::class_name = "StudentClass";
+
+int main () {
+  CacheManager<Student> my_cache(3);
+
+  my_cache.insert("0", Student(0, 22, "student1"));
+  cout << "todo funka piola" << endl;
   return 0;
 }
