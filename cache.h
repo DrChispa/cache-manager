3b288b52c410ad657ab87df28ac7a86428988eea
emiliano <emiliano.d.sandoval1@gmail.com>
2025-08-04 01:36:13 +0000

    Finalizado

--- cache.h
+++ cache.h
@@ -1,120 +1,123 @@
-#include <iostream>
 #include <fstream>
-#include <map>
-#include <utility>
-#include <string>
+#include <iostream>
 #include <limits>
+#include <map>
+#include <string>
+#include <utility>
 
 using namespace std;
 
-template<class T>
-class CacheManager {
-  //members (private)
+template <class T> class CacheManager {
+  // members (private)
 private:
   size_t capacity;
-  map<string, pair<T, int>> cache_data; 
+  map<string, pair<T, int>> cache_data;
   int current_mru = 0;
-  bool write_file (string, T);
+  bool write_file(string, T);
 
 public:
   CacheManager(int);
   ~CacheManager();
-  
-  void insert (string key, T obj);
-  T get (string key);
+
+  void insert(string key, T obj);
+  T get(string key);
   void show_cache() const;
 };
 
-template <class T>
-CacheManager<T>::CacheManager(int cap) {
-  capacity = cap;
-}
+template <class T> CacheManager<T>::CacheManager(int cap) { capacity = cap; }
+
+template <class T> CacheManager<T>::~CacheManager() {}
 
 template <class T>
-CacheManager<T>::~CacheManager() {}
-
-template <class T>
-bool CacheManager<T>::write_file (const string key,const T obj) {
+bool CacheManager<T>::write_file(const string key, const T obj) {
 
   // actualizamos la el objeto dentro de la funcion
-  
-  ofstream file("cache_data.txt", ios::app);
-  if(!file){
+  map<string, T> file_content;
+  ifstream infile("cache_data.txt");
+  string current_key;
+  T current_obj;
+
+  while (infile >> current_key >> current_obj) {
+    file_content[current_key] = current_obj;
+  }
+  infile.close();
+
+  // insertar nuevo elemento
+  file_content[key] = obj;
+  ofstream outfile("cache_data.txt");
+  if (!outfile) {
     cerr << "Error al abrir el archivo para escritura" << endl;
     return false;
   }
 
-  file << key << " " << obj << "\n";
+  for (const auto &item : file_content) {
+    outfile << item.first << " " << item.second << endl;
+  }
 
-  file.close();
-return true;
+  outfile.close();
+  return true;
 }
 
-
-template <class T>
-void CacheManager<T>::insert (string key, T obj) {
+template <class T> void CacheManager<T>::insert(string key, T obj) {
   // Si la clave existe, actualizamos
   auto it = cache_data.find(key);
   if (it != cache_data.end()) {
-    it -> second.first = obj;
-    it -> second.second = ++current_mru;
+    it->second.first = obj;
+    it->second.second = ++current_mru;
   } else {
     // si la cache esta llena, eliminamos el LRU
     if (cache_data.size() >= capacity) {
-      auto lru_it= cache_data.begin();
-      
-      //buscamos el elemento con el MRU mas alto(el menos reciente)
+      auto lru_it = cache_data.begin();
+
+      // buscamos el elemento con el MRU mas alto(el menos reciente)
       for (auto it = cache_data.begin(); it != cache_data.end(); ++it) {
-        if (it -> second.second < lru_it-> second.second) {
+        if (it->second.second < lru_it->second.second) {
           lru_it = it;
         }
       }
-    cache_data.erase(lru_it);
+      cache_data.erase(lru_it);
     }
-cache_data[key] = make_pair(obj, ++current_mru);
-  } 
-    //insertamos el nuevo elemento con MRU = 0 (el mas reciente)
-write_file(key, obj);
+    cache_data[key] = make_pair(obj, ++current_mru);
+  }
+  // insertamos el nuevo elemento con MRU = 0 (el mas reciente)
+  write_file(key, obj);
 }
 
-template<class T>
-T CacheManager<T>::get (string key) {
-  //Busquea  en la cache
+template <class T> T CacheManager<T>::get(string key) {
+  // Busquea  en la cache
   auto it = cache_data.find(key);
   if (it != cache_data.end()) {
-      it -> second.second = ++current_mru;
-      return it -> second.first; // el mas reciente
+    it->second.second = ++current_mru;
+    return it->second.first; // el mas reciente
   }
-   //Busqueda en archivo
+  // Busqueda en archivo
   ifstream file("cache_data.txt");
-  if (!file){
-      throw runtime_error("Archivo no encontrado");
+  if (!file) {
+    throw runtime_error("Archivo no encontrado");
   }
 
   string file_key;
   T found_obj;
-  while(file >> file_key) {
-      if(file_key == key){
-        file >> found_obj;
-        insert(key, found_obj);
-        return found_obj;
-      }
-      file.ignore(numeric_limits<streamsize>::max(), '\n');
+  while (file >> file_key) {
+    if (file_key == key) {
+      file >> found_obj;
+      return found_obj;
+    }
+    file.ignore(numeric_limits<streamsize>::max(), '\n');
   }
   throw runtime_error("Clave no encontrada");
 }
 
-template <class T>
-void CacheManager<T>::show_cache() const {
-    cout << "\n=== Contenido de la Cache ===" << endl;
-    cout << "Capacidad: " << capacity << endl;
-    cout << "Indice | Dato | MRU/LRU\n";
+template <class T> void CacheManager<T>::show_cache() const {
+  cout << "\n=== Contenido de la Cache ===" << endl;
+  cout << "Capacidad: " << capacity << endl;
+  cout << "Indice | Dato | MRU/LRU\n";
+  cout << "---------------------------" << endl;
+
+  for (const auto &item : cache_data) {
+    cout << item.first << " | ";
+    item.second.first.print();
+    cout << " | " << item.second.second << endl;
     cout << "---------------------------" << endl;
-    
-    for (const auto& item : cache_data) {
-        cout << item.first << " | ";
-        item.second.first.print();
-        cout << " | " << item.second.second << endl;
-        cout << "---------------------------" << endl;
-    }
+  }
 }
--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include "cache.h"
-#include <string>
 #include <iostream>
+#include <string>
 
 using namespace std;
 
@@ -8,47 +8,54 @@ class Student {
   int id;
   int value;
   string data;
+
 public:
   static const string class_name;
- // Constructor por default
-Student() : id(0), value(0), data("") {}
+  // Constructor por default
+  Student() : id(0), value(0), data("") {}
 
-  Student(int _key, int _value, const char * _data): id (_key),value (_value),data (_data) {}
-  
-  friend ostream& operator<<(ostream& os, const Student& a) { 
-   return os << a.id << " " << a.value << " " << a.data;
+  Student(int _key, int _value, const char *_data)
+      : id(_key), value(_value), data(_data) {}
+
+  friend ostream &operator<<(ostream &os, const Student &a) {
+    return os << a.id << " " << a.value << " \"" << a.data << "\"";
   }
-  friend istream& operator>>(istream& is, Student& a) {
-   return is >> a.id >> a.value >> a.data;
+  friend istream &operator>>(istream &is, Student &a) {
+    char quote;
+    is >> a.id >> a.value >> quote;
+    getline(is, a.data, '"');
+    return is;
   }
-  void print() const {
-    cout << data; 
+  void print() const { cout << data; }
+  ~Student() {};
+
+  void limpiarArchivo() {
+    ofstream file("cache_data.txt");
+    file.close();
   }
-  ~Student () {};
 };
 
 const string Student::class_name = "StudentClass";
 
-int main () {
+int main() {
+  remove("cache_data.txt");
   CacheManager<Student> my_cache(3);
-
-  my_cache.insert("a", Student(0,22,"Student1"));
+  my_cache.insert("a", Student(0, 22, "Student1"));
   my_cache.show_cache();
-  my_cache.insert("b", Student(1,33,"Student2"));
+  my_cache.insert("b", Student(1, 33, "Student2"));
   my_cache.show_cache();
-  my_cache.insert("c", Student(2,44,"Student3"));
+  my_cache.insert("c", Student(2, 44, "Student3"));
   my_cache.show_cache();
-  my_cache.insert("h", Student(0,22,"Student1"));
+  my_cache.insert("h", Student(3, 22, "Student1"));
   my_cache.show_cache();
-  my_cache.insert("f", Student(3,55,"Student4"));
-
+  my_cache.insert("a", Student(4, 22, "Student4"));
   my_cache.show_cache();
 
   cout << "---------------------UPDATE-----------------" << endl;
-  my_cache.insert("2", Student(2,222,"EZE"));
+  my_cache.insert("2", Student(2, 222, "EZE"));
   my_cache.show_cache();
 
   cout << "---------------------GET-----------------" << endl;
   Student return_obj = my_cache.get("a");
- return 0;
+  return 0;
 }
